6a14fff5807f2d74423bec7034c526260eaf9cb0
Miroslav Bartyzal <miroslavbartyzal@gmail.com>
2021-02-05 20:38:44 +0100

    first commit

---
+++ src/main/java/cz/miroslavbartyzal/splashscreenbug/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.miroslavbartyzal.splashscreenbug;
+
+/**
+ *
+ * @author Miroslav Bartyzal (miroslavbartyzal@gmail.com)
+ */
+public class Main
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        Thread.sleep(2000);
+    }
+}
